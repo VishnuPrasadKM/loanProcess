1ba1ad702e656d182483bb1f35044fdc83ca0e81
VishnuPrasadKM <kmvishnuprasad@gmail.com>
2022-10-21 14:37:05 +0530

    rate-lock commit

--- front/src/Components/rateLock.js
+++ front/src/Components/rateLock.js
@@ -6,38 +6,48 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Col, Container, Form, Row } from 'react-bootstrap';
 import {AiOutlineFullscreen} from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom';
+
 const RateLock=() =>{
+
+  const navigate = useNavigate()
   const [show, setShow] = useState(true);
   const [full, setFull] = useState(false)
-  const [date, setDate] = useState(null);
 
-  const handleClose = () => setShow(false);
+  const handleClose = (e) => {
+    setShow(false)
+    navigate('/')
+    e.preventDefault()
+  };
   const handleShow = () => setShow(true);
   const handleScreen = () => setFull(false)
   const fullscreen = () => setFull(true)
-  return (
-    <>
-      <Header/>
-      <div style={{margin:'5px 45px'}}>
-      
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-      <Modal show={show} onHide={handleClose} size='xl' centered >
-          <Container>
-            <Modal.Header closeButton style={{borderBottom:0, padding:'10px 0 0 0'}}>
-            <Modal.Title id="contained-modal-title-vcenter" >
-              <div style={{fontSize:'14px', fontWeight:500}}>
-                <AiOutlineHome/> <IoIosArrowForward/> Rate Lock
-                </div>
-            </Modal.Title>
-            </Modal.Header>
-            <div style={{display:'flex', justifyContent:'space-between'}}>
-              <h3 className='pt-2'>Rate Lock</h3>
-              {/* <button style={{alignItems:'center', background:'inherit', border:'none' }} onClick={{fullscreen}}><AiOutlineFullscreen/> FULLSCREEN</button> */}
-            </div>
-            <Modal.Body>
-              <p className='text-muted' style={{fontSize:'15px'}}>Status</p>
+
+  const ModalTitle=()=>{
+    return(
+      <>
+        <div style={{fontSize:'14px', fontWeight:500}}>
+          <AiOutlineHome/> <IoIosArrowForward/> Rate Lock
+        </div>
+      </>
+    )
+  }
+
+  const ModalMiddle=()=>{
+    return(
+      <>
+        <div style={{display:'flex', justifyContent:'space-between'}}>
+          <h3 className='pt-2'>Rate Lock</h3>
+          <button style={{alignItems:'center', background:'inherit', border:'none' }} onClick={()=>navigate('/ratelock-fullscreen')}><AiOutlineFullscreen/> FULLSCREEN</button>
+        </div>
+      </>
+    )
+  }
+
+  const ModalBody=()=>{
+    return(
+      <>
+      <p className='text-muted' style={{fontSize:'15px'}}>Status</p>
               <Form style={{marginLeft:'8px'}}>
                 <Form.Check
                   inline
@@ -55,10 +65,11 @@ const RateLock=() =>{
                   id='lock'
                 />
               </Form>
+              <form id='rateLock' style={{marginLeft:'0px'}}>
                 <Row className='mt-3'>
                     <Row>
-                      <Col lg={4}><label htmlFor='valid'>Valid From *</label></Col>
-                      <Col lg={4}><label htmlFor='expire'>Expires On *</label></Col>
+                      <Col lg={4}><label htmlFor='valid' required>Valid From *</label></Col>
+                      <Col lg={4}><label htmlFor='expire' required>Expires On *</label></Col>
                       <Col lg={4}><label htmlFor='ftp'>FTP</label></Col>
                     </Row>
                     <Row className='modalInput'>
@@ -73,12 +84,31 @@ const RateLock=() =>{
                       <input type='text' id='notes' />
                     </Row>
                 </Row>
+              </form>
+    </>
+    )
+  }
+
+  return (
+    <>
+      <Header/>
+      <div style={{margin:'5px 45px'}}>
+      <Modal show={show} onHide={handleClose} size='xl' centered >
+          <Container>
+            <Modal.Header closeButton style={{borderBottom:0, padding:'10px 0 0 0'}}>
+            <Modal.Title id="contained-modal-title-vcenter" >
+              {ModalTitle()}
+            </Modal.Title>
+            </Modal.Header>
+              {ModalMiddle()}
+            <Modal.Body>
+              {ModalBody()}
             </Modal.Body>
             <Modal.Footer style={{borderTop:0}}>
               <Button variant="secondary" onClick={handleClose}>
                 CANCEL
               </Button>
-              <Button variant="primary" onClick={handleClose}>
+              <Button type='submit' form='rateLock'  onClick={handleClose}>
                 SAVE CHANGES
               </Button>
             </Modal.Footer>
