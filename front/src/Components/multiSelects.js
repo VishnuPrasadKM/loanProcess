cfde0ca4f924077a34b7cadf2accb3df9903375c
VishnuPrasadKM <kmvishnuprasad@gmail.com>
2022-11-03 21:56:23 +0530

    multiselect component

--- front/src/Components/multiSelects.js
+++ front/src/Components/multiSelects.js
@@ -2,52 +2,54 @@ import React from 'react'
 import {FaCheck, FaChevronDown} from 'react-icons/fa'
 const multiSelects = () => {
   return (
-    <div style={{width:'50px'}}>
-    <button>
-        <span class="btn-text">COLUMN</span>
+    <div>
+        <div class="container">
+    <div class="select-btn">
+        <span class="btn-text">Select Language</span>
         <span class="arrow-dwn">
-            <FaChevronDown/>
+            <i class="fa-solid fa-chevron-down"></i>
         </span>
-    </button>
+    </div>
 
     <ul class="list-items">
         <li class="item">
             <span class="checkbox">
-                <FaCheck/>
+                <i class="fa-solid fa-check check-icon"></i>
             </span>
             <span class="item-text">HTML & CSS</span>
         </li>
         <li class="item">
             <span class="checkbox">
-                <FaCheck/>
+                <i class="fa-solid fa-check check-icon"></i>
             </span>
             <span class="item-text">Bootstrap</span>
         </li>
         <li class="item">
             <span class="checkbox">
-                <FaCheck/>
+                <i class="fa-solid fa-check check-icon"></i>
             </span>
             <span class="item-text">JavaScript</span>
         </li>
         <li class="item">
             <span class="checkbox">
-                <FaCheck/>
+                <i class="fa-solid fa-check check-icon"></i>
             </span>
             <span class="item-text">Node.Js</span>
         </li>
         <li class="item">
             <span class="checkbox">
-                <FaCheck/>
+                <i class="fa-solid fa-check check-icon"></i>
             </span>
             <span class="item-text">React JS</span>
         </li>
         <li class="item">
             <span class="checkbox">
-                <FaCheck/>
+                <i class="fa-solid fa-check check-icon"></i>
             </span>
             <span class="item-text">Mango DB</span>
         </li>
     </ul>
+</div>
 </div>
   )
 }
