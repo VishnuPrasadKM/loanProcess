47d4900ddac85f92523b2c46a34500bf40e55195
VishnuPrasadKM <kmvishnuprasad@gmail.com>
2022-12-23 13:27:10 +0530

    logo added

--- front/src/Components/Reusable/header.js
+++ front/src/Components/Reusable/header.js
@@ -1,12 +1,12 @@
 import Navbar from 'react-bootstrap/Navbar';
+import { TbBuildingBank } from 'react-icons/tb';
 
 function Header() {
   return (
     <div  >
     <Navbar expand="lg" className='header'>
-        <Navbar.Brand style={{fontSize:'25px', fontWeight:'500', color:'white'}}>LOGO</Navbar.Brand>
-            <Navbar.Text className='divider' style={{color:'white'}}>|</Navbar.Text>
-            <Navbar.Text style={{color:'white'}}>Project Name</Navbar.Text>
+        <Navbar.Brand style={{fontSize:'25px', fontWeight:'500', color:'white', display:'flex'}}><span style={{display:'flex', fontSize:'larger'}}><TbBuildingBank/></span>LOGO</Navbar.Brand>
+            <Navbar.Text style={{borderLeft:"2px solid white" ,color:'white', paddingLeft:'18px' }}>Project Name</Navbar.Text>
     </Navbar>
     </div>
   );
